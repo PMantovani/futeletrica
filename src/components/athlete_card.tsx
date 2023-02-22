fc660638aefd2f75a0efe908669557faa582d763
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2023-02-22 16:32:34 -0300

    feat: add animation to athlete card

--- src/components/athlete_card.tsx
+++ src/components/athlete_card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Athlete } from "@/models/athlete";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Color } from "@/models/color";
 
 interface Props {
@@ -10,20 +10,46 @@ interface Props {
 }
 
 export const AthleteCard: React.FC<Props> = (props) => {
-  let imgSrc;
+  const [imgSrc, setImgSrc] = useState(
+    require(`public/assets/athlete_cards/white/fallback.png`)
+  );
   let isFallback = false;
-  try {
-    imgSrc = require(`public/assets/athlete_cards/${props.color}/${props.athlete.id}.png`);
-  } catch (err) {
-    isFallback = true;
-    imgSrc = require(`public/assets/athlete_cards/${props.color}/fallback.png`);
-  }
+
+  const [isInitial, setIsInitial] = useState(true);
+
+  const recalculateImgSrc = () => {
+    setIsInitial(false);
+    try {
+      setImgSrc(
+        require(`public/assets/athlete_cards/${props.color}/${props.athlete.id}.png`)
+      );
+    } catch (err) {
+      isFallback = true;
+      setImgSrc(
+        require(`public/assets/athlete_cards/${props.color}/fallback.png`)
+      );
+    }
+  };
+
+  useEffect(() => {
+    recalculateImgSrc();
+  }, []);
+
+  useEffect(() => {
+    setIsInitial(true);
+    setTimeout(() => recalculateImgSrc(), 500);
+  }, [props.color]);
 
   switch (props.idx) {
     case 0:
       return (
         <div
-          className={`absolute top-[53%] left-[5%] w-[30%] md:left-[25%] md:w-[15%]`}
+          className={
+            `absolute w-[30%] transition-all duration-500 md:w-[15%] ` +
+            (isInitial ? `top-[30%] ` : `top-[53%] `) +
+            (isInitial ? `left-[35%] ` : `left-[5%] `) +
+            (isInitial ? `md:left-[42.5%] ` : `md:left-[25%] `)
+          }
         >
           <Image src={imgSrc} alt="Card do atleta"></Image>
           {isFallback && (
@@ -34,7 +60,12 @@ export const AthleteCard: React.FC<Props> = (props) => {
     case 1:
       return (
         <div
-          className={`absolute top-[53%] left-[65%] w-[30%] md:left-[60%] md:w-[15%]`}
+          className={
+            `absolute w-[30%] transition-all duration-500 md:w-[15%] ` +
+            (isInitial ? `top-[30%] ` : `top-[53%] `) +
+            (isInitial ? `left-[35%] ` : `left-[65%] `) +
+            (isInitial ? `md:left-[42.5%] ` : `md:left-[60%] `)
+          }
         >
           <Image src={imgSrc} alt="Card do atleta"></Image>
           {isFallback && (
@@ -45,7 +76,13 @@ export const AthleteCard: React.FC<Props> = (props) => {
     case 2:
       return (
         <div
-          className={`absolute top-[17%] left-[5%] w-[30%] md:left-[25%] md:w-[15%] xl:top-[10%]`}
+          className={
+            `absolute w-[30%] transition-all duration-500 md:w-[15%] ` +
+            (isInitial ? `top-[30%] ` : `top-[17%] `) +
+            (isInitial ? `xl:top-[30%] ` : `xl:top-[10%] `) +
+            (isInitial ? `left-[35%] ` : `left-[5%] `) +
+            (isInitial ? `md:left-[42.5%] ` : `md:left-[25%] `)
+          }
         >
           <Image src={imgSrc} alt="Card do atleta"></Image>
           {isFallback && (
@@ -57,7 +94,13 @@ export const AthleteCard: React.FC<Props> = (props) => {
     case 3:
       return (
         <div
-          className={`absolute top-[17%] left-[65%] w-[30%] md:left-[60%] md:w-[15%] xl:top-[10%]`}
+          className={
+            `absolute w-[30%] transition-all duration-500 md:w-[15%] ` +
+            (isInitial ? `top-[30%] ` : `top-[17%] `) +
+            (isInitial ? `xl:top-[30%] ` : `xl:top-[10%] `) +
+            (isInitial ? `left-[35%] ` : `left-[65%] `) +
+            (isInitial ? `md:left-[42.5%] ` : `md:left-[60%] `)
+          }
         >
           <Image src={imgSrc} alt="Card do atleta"></Image>
           {isFallback && (
@@ -69,7 +112,13 @@ export const AthleteCard: React.FC<Props> = (props) => {
     case 4:
       return (
         <div
-          className={`absolute top-[5%] left-[35%] w-[30%] md:left-[42.5%] md:w-[15%] xl:top-[1%]`}
+          className={
+            `absolute w-[30%] transition-all duration-500 md:w-[15%] ` +
+            (isInitial ? `top-[30%] ` : `top-[5%] `) +
+            (isInitial ? `xl:top-[30%] ` : `xl:top-[1%] `) +
+            (isInitial ? `left-[35%] ` : `left-[35%] `) +
+            (isInitial ? `md:left-[42.5%] ` : `md:left-[42.5%] `)
+          }
         >
           <Image src={imgSrc} alt="Card do atleta"></Image>
           {isFallback && (
@@ -88,7 +137,7 @@ const FallbackName: React.FC<{ name: string; color: Color }> = ({
   return (
     <div
       className={
-        `absolute top-[56%] mx-auto w-full px-[10%] text-center font-bold uppercase xl:text-xl ` +
+        `absolute top-[56%] mx-auto w-full px-[10%] text-center font-bold uppercase transition-all duration-500 xl:text-xl ` +
         `${
           color === "white"
             ? "text-whiteCardText"
