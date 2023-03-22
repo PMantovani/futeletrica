65110f819ec8aeede9fae1331a44aae0957a21cc
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2023-03-21 23:09:46 -0300

    fix: fix overflow of main component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 export const Main: React.FC<PropsWithChildren> = (props) => (
-  <main className="flex h-screen flex-col bg-neutral-900 text-yellow">
+  <main className="flex h-screen flex-col overflow-auto bg-neutral-900 text-yellow">
     {props.children}
   </main>
 );
