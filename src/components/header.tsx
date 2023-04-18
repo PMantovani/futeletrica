2fa410fdcdb7bd500f3f2e1c90ec089610df1cb0
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2023-04-18 16:54:04 -0300

    feat: remove back button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import logo from "public/images/logo.png";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
-export const Header: React.FC<{}> = () => {
-  const router = useRouter();
-
-  const onBackButton = () => {
-    router.back();
-  };
-
-  return (
-    <div className="flex items-center p-4">
-      {router.pathname !== "/" && (
-        <button onClick={onBackButton} className="mr-4 text-yellow">
-          Voltar
-        </button>
-      )}
-      <Image
-        alt="Logo do Fut Elétrica 2013"
-        src={logo}
-        width="56"
-        height="67"
-      />
-      <h1 className=" ml-4 text-2xl text-yellow">Fut Elétrica 2013</h1>
-    </div>
-  );
-};
+export const Header: React.FC<{}> = () => (
+  <Link href={"/"} className="flex items-center p-4">
+    <Image alt="Logo do Fut Elétrica 2013" src={logo} width="56" height="67" />
+    <h1 className=" ml-4 text-2xl text-yellow">Fut Elétrica 2013</h1>
+  </Link>
+);
