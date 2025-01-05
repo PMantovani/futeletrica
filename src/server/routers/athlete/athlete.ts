f15a17ae83cca10803d4e3441c67da65b52525c9
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2025-01-05 11:59:49 -0300

    chore: update to season 4

--- src/server/routers/athlete/athlete.ts
+++ src/server/routers/athlete/athlete.ts
@@ -4,8 +4,8 @@ import { z } from "zod";
 
 export const athleteRouter = router({
   findAll: procedure.query(() => findAllAthletes()),
-  getAthleteStandings: procedure.query(() => getAthleteStandings(2)),
+  getAthleteStandings: procedure.query(() => getAthleteStandings(4)),
   getAthleteSeasonSummary: procedure
     .input(z.number({ required_error: "Enter an athlete id" }))
-    .query((params) => getAthleteSeasonSummary(2, params.input)),
+    .query((params) => getAthleteSeasonSummary(4, params.input)),
 });
